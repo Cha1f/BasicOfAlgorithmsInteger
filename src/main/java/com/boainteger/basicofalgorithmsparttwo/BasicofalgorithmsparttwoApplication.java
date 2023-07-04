18665e65c050dc0ab7a26718db72f164cce61fbf
Рома <pomo4444ka@gmail.com>
2023-07-04 20:48:10 +0300

    Начальный коммит

---
+++ src/main/java/com/boainteger/basicofalgorithmsparttwo/BasicofalgorithmsparttwoApplication.java
@@ -0,0 +1,13 @@
+package com.boainteger.basicofalgorithmsparttwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicofalgorithmsparttwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicofalgorithmsparttwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boainteger/basicofalgorithmsparttwo/BasicofalgorithmsparttwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boainteger.basicofalgorithmsparttwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicofalgorithmsparttwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
